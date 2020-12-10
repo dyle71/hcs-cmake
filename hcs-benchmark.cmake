a73b9c55592dab50a8edcb2981736f31d97ca1ec
Oliver Maurhart <oliver.maurhart@headcode.space>
2020-12-10 14:09:54 +0100

    Adds headcode.space benchmark pull in shortcut

---
+++ hcs-benchmark.cmake
@@ -0,0 +1,45 @@
+# ------------------------------------------------------------
+# This file defines the CMake for headcode-benchmark integration
+#
+# The 'LICENSE.txt' file in the project root holds the software license.
+# Copyright (C) 2020 headcode.space
+# https://www.headcode.space, <info@headcode.space>
+# ------------------------------------------------------------
+
+
+# ------------------------------------------------------------
+# if you do not have hcs-benchmark installed or downloaded, do a
+#       $ git submodule init
+#       $ git submodule add https://gitlab.com/headcode.space/benchmark.git 3rd/hcs-benchmark
+# in the project root.
+# ------------------------------------------------------------
+
+if (NOT DEFINED HCS_BENCHMARK_SOURCE_DIR)
+    set(HCS_BENCHMARK_SOURCE_DIR ${CMAKE_SOURCE_DIR}/3rd/hcs-benchmark)
+endif()
+
+set(CMAKE_REQUIRED_QUIET_PREVIOUS ${CMAKE_REQUIRED_QUIET})
+set(CMAKE_REQUIRED_QUIET "on")
+CHECK_INCLUDE_FILE_CXX("headcode/benchmark/benchmark.hpp" HCS_BENCHMARK_INCLUDE)
+set(CMAKE_REQUIRED_QUIET ${CMAKE_REQUIRED_QUIET_PREVIOUS})
+
+if ("${HCS_BENCHMARK_INCLUDE}" STREQUAL "")
+
+    if (NOT EXISTS "${HCS_BENCHMARK_SOURCE_DIR}/CMakeLists.txt")
+        message(FATAL_ERROR "The git submodule for hcs-benchmark has not been downloaded!")
+    endif ()
+
+    message(STATUS "Using hcs-benchmark found at ${HCS_BENCHMARK_SOURCE_DIR}...")
+
+    execute_process(COMMAND "${CMAKE_COMMAND}"
+            -S "${HCS_BENCHMARK_SOURCE_DIR}"
+            -B ${CMAKE_BINARY_DIR}/3rd/hcs-benchmark
+    )
+    execute_process(COMMAND "${CMAKE_COMMAND}" --build ${CMAKE_BINARY_DIR}/3rd/hcs-benchmark
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/3rd/hcs-benchmark
+    )
+
+    set(HCS_BENCHMARK_INCLUDE_DIR "${HCS_BENCHMARK_SOURCE_DIR}/include")
+    set(HCS_BENCHMARK_LIB_DIR "")
+
+endif ()
---
+++ hcs-benchmark.cmake.md
@@ -0,0 +1,49 @@
+# hcs-benchmark.cmake
+
+Helper to pull in the headcode.space benchmark utilities.
+
+
+## Synopsis
+
+Install the headcode.space benchmark utilities on the system and add the path to the includes.
+
+* `HCS_BENCHMARK_SOURCE_DIR`: path to the headcode.space benchmark sources.
+* `HCS_BENCHMARK_INCLUDE_DIR`: location of the headcode.space benchmark include files.
+
+If you lack the above, add the headcode.space to your project and do a
+```bash
+$ mkdir 3rd
+$ git submodule init
+$ git submodule add https://gitlab.com/headcode.space/benchmark.git 3rd/hcs-benchmark
+```
+in your project root. Finally point the `HCS_BENCHMARK_SOURCE_DIR` to this path in your `CMakeLists.txt`.
+
+
+## Options
+
+
+Define all of these: `HCS_BENCHMARK_SOURCE_DIR` *or* download the headcode.space benchmark into a subfolder as 
+depicted above.
+
+
+## Example
+
+Download Google Test as submodule like this:
+
+```bash
+$ mkdir 3rd
+$ git submodule init
+$ git submodule add https://gitlab.com/headcode.space/benchmark.git 3rd/hcs-benchmark
+```
+
+```
+include(hcs-benchmark)
+include_directories(${HCS_BENCHMAKR_INCLUDE_DIR})
+```
+
+
+---
+
+Oliver Maurhart, <oliver.maurhart@headcode.space>  
+(C)opyright 2020 headcode.space  
+[https://headcode.space](https://www.headcode.space)
