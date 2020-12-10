fcf2e2a941bf6af45cb941f6255d379481f2c017
Oliver Maurhart <oliver.maurhart@headcode.space>
2020-12-10 15:47:59 +0100

    Drops requirement for an access token in gitlab push

--- gitlab-package-push.cmake
+++ gitlab-package-push.cmake
@@ -6,22 +6,18 @@
 # https://www.headcode.space, <info@headcode.space>
 # ------------------------------------------------------------
 
-if (DEFINED GITLAB_ACCESS_TOKEN)
-
-    if ("${GITLAB_PACKAGE_URL}" STREQUAL "")
-        message(FATAL_ERROR "Cannot push a package without a destination url. Please specify GITLAB_PACKAGE_URL.")
-    endif()
-    if ("${PACKAGE_FILE}" STREQUAL "")
-        message(FATAL_ERROR "Don't know what to push. Please specify PACKAGE_FILE.")
-    endif()
-    if ("${PACKAGE_FILE_NAME}" STREQUAL "")
-        message(FATAL_ERROR "Cannot push a package without file name. Please specify PACKAGE_FILE_NAME.")
-    endif()
-
-    find_program(CURL_COMMAND curl PATHS /usr/bin /usr/local/bin REQUIRED)
-    add_custom_target(gitlab_push_package
-        COMMAND ${CURL_COMMAND} --header "JOB-TOKEN: $CI_JOB_TOKEN" --upload-file "${PACKAGE_FILE}" "${GITLAB_PACKAGE_URL}/${CPACK_PROJECT_VERSION_STRING}/${PACKAGE_FILE_NAME}"
-        COMMENT "PUT ${PACKAGE_FILE_NAME} ==> ${GITLAB_PACKAGE_URL}/${CPACK_PROJECT_VERSION_STRING}/${PACKAGE_FILE_NAME}..."
-    )
-
+if ("${GITLAB_PACKAGE_URL}" STREQUAL "")
+    message(FATAL_ERROR "Cannot push a package without a destination url. Please specify GITLAB_PACKAGE_URL.")
 endif()
+if ("${PACKAGE_FILE}" STREQUAL "")
+    message(FATAL_ERROR "Don't know what to push. Please specify PACKAGE_FILE.")
+endif()
+if ("${PACKAGE_FILE_NAME}" STREQUAL "")
+    message(FATAL_ERROR "Cannot push a package without file name. Please specify PACKAGE_FILE_NAME.")
+endif()
+
+find_program(CURL_COMMAND curl PATHS /usr/bin /usr/local/bin REQUIRED)
+add_custom_target(gitlab_push_package
+    COMMAND ${CURL_COMMAND} --header "JOB-TOKEN: $CI_JOB_TOKEN" --upload-file "${PACKAGE_FILE}" "${GITLAB_PACKAGE_URL}/${CPACK_PROJECT_VERSION_STRING}/${PACKAGE_FILE_NAME}"
+    COMMENT "PUT ${PACKAGE_FILE_NAME} ==> ${GITLAB_PACKAGE_URL}/${CPACK_PROJECT_VERSION_STRING}/${PACKAGE_FILE_NAME}..."
+)
